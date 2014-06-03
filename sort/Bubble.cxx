b949a6881b04b8a9e91c1251de4c127cbd8a6a01
Zhu Shuai <zhushuai19890918@gmail.com>
2014-06-03 21:02:33 +0800

    modify sort

--- sort/Bubble.cxx
+++ sort/Bubble.cxx
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<iomanip>
 
 using namespace std;
 void bubbleSort(int a[], int size);
@@ -7,15 +8,17 @@ int main()
 {
 	int array[] = {34, 65, 12, 43, 67, 5, 78, 10, 3, 70}, k;
 	int len = sizeof(array)/sizeof(int);
-	cout<<"The original array are: "<<endl;
+	cout<<setw(3)<<"The original array are: "<<endl;
 	for(k=0; k<len; k++)
-		cout<<array[k]<<", ";
-	cout<<endl;
+		cout<<setw(3)<<array[k]<<", ";
+	cout<<setw(3)<<endl;
+	cout<<setw(3)<<endl;
 	bubbleSort(array, len);
-	cout<<"The sorted array are: "<<endl;
+	cout<<setw(3)<<"The sorted array are: "<<endl;
 	for(k=0; k<len; k++)
-		cout<<array[k]<<", ";
-	cout<<endl;
+		cout<<setw(3)<<array[k]<<", ";
+	cout<<setw(3)<<endl;
+	cout<<setw(3)<<endl;
 //	system("pause");
 	return 0;
 }
@@ -32,9 +35,9 @@ void bubbleSort(int a[], int size)
 				a[k] = a[k+1];
 				a[k+1] = temp;
 			}
-		cout<<"第"<<pass<<"轮排序后: ";
+		cout<<setw(3)<<"第"<<pass<<"轮排序后: ";
 		for(int i=0; i<size; i++)
-			cout<<a[i]<<", ";
-		cout<<endl;
+			cout<<setw(3)<<a[i]<<", ";
+		cout<<setw(3)<<endl;
 	}
 }
